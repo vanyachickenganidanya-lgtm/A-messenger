c4863765fb35a0b64cf3bd2bd27bdcbcef6bbb74
vanyachickenganidanya-lgtm <vanya.chicken.gan.i.danya@gmail.com>
2026-07-21 12:35:08 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 echo "=== Запуск ngIRCd ==="
-# Запуск IRC-сервера в фоне
 ngircd
 
 echo "=== Запуск веб-заглушки для Render на порту $PORT ==="
 python3 -m http.server $PORT &
 
-echo "=== Запуск туннеля Pinggy ==="
-# -T отключает псевдо-терминал (уберет ошибку Pseudo-terminal in logs)
-# -p 443 использует безопасный SSL-порт, который никогда не блокируется
-# -R0:localhost:6667 запрашивает случайный TCP-порт для проброса нашего IRC
-ssh -T -o StrictHostKeyChecking=no -p 443 -R0:localhost:6667 tcp@pinggy.io
+echo "=== Запуск туннеля Bore ==="
+# Пробрасываем локальный порт 6667 на бесплатный сервер bore.pub
+bore local 6667 --to bore.pub
